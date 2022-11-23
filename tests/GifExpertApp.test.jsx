883ab7338bf37cc6e9e06020946d8757656b1874
Arturo Gonzalez <arturo.gonzalez@rtasoftware.com.mx>
2022-11-23 17:45:24 -0600

    test script fixed

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -6,15 +6,12 @@ describe('Tests in <GifExpertApp />', () => {
     test('Should show initial state with no gifs', () => { 
 
         render(<GifExpertApp />);
-        screen.debug();
 
         const element = screen.getByRole('textbox');
         const form = screen.getByRole('form');
 
         fireEvent.input(element, { target: { value: 'Goku' } });
         fireEvent.submit(form);
-
-        screen.debug();
 
     });
 
